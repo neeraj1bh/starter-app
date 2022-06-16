4f9bf9c2706c9a41288649b9da0e524bcaa3a97e
Neeraj Bhatt <neeraj1bh@gmail.com>
2022-06-16 17:21:48 +0530

    chore: added circle ci gcp fix test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello GCP from NestJS!');
+      expect(appController.getHello()).toBe('Hello GCP 123 from NestJS!');
     });
   });
 });
